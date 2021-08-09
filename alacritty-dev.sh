55b71b3755456a4e1a2494997373c21dc75919b3
rick <rick@ricks-MacBook-Air.local>
2021-08-09 15:56:46 -0400

    add fonts script

--- alacritty-dev.sh
+++ alacritty-dev.sh
@@ -3,8 +3,10 @@ set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source ansi.sh
 name="$1"
+MANAGED_HOST="${MANAGED_HOST:-f29}"
+
 ALACRITTY_BINARY=/Applications/Alacritty.app/Contents/MacOS/alacritty
-ALACRITTY_CONFIG=~/.alacritty-f29.yml
+ALACRITTY_CONFIG=~/.alacritty-$MANAGED_HOST.yml
 ALACRITTY_WINDOW_STARTUP_MODE=Windowed
 X=0
 Y=0
@@ -20,25 +22,41 @@ if [[ "$name" == "right" ]]; then other_name=left; fi
 
 cur_app="$(./get_current_active_application.sh)"
 
+#ALACRITTY_FONT="Fira Code"
+#ALACRITTY_FONT="Consolas"
+#ALACRITTY_FONT="Menlo"
 if [[ "$name" == "left" ]]; then
   ALACRITTY_FONT="Monaco"
+  FONT_STYLE="Regular"
   X=0
   Y=0
   COLS=94
   LINES=42
 elif [[ "$name" == "right" ]]; then
-  ALACRITTY_FONT="Menlo"
   X=1600
   Y=0
-  COLS=107
-  LINES=42
+  COLS=122
+  LINES=49
+  ALACRITTY_FONT="Inconsolata"
+  FONT_STYLE="Light"
 fi
-ALACRITTY_FONT="Fira Code"
 
 
-exec_name="alacritty-$name-dev"
+
+
+
+set -x
+
+exec_name="${MANAGED_HOST}...${name}"
 APP_ICON=./icons/$exec_name.png
-APP_ICON_ICNS=$APP_ICON.icns
+APP_ICON_ICNS=./icons/.${APP_ICON}.icns
+
+
+
+#exit
+
+
+
 
 if [[ -f "$APP_ICON" ]]; then
   if [[ ! -f "$APP_ICON_ICNS" ]]; then
@@ -62,6 +80,7 @@ else
 fi
 
 tf="$(mktemp -d)/$exec_name"
+#_$MANAGED_HOST"
 
 rm_tf(){
  [[ -f $tf ]] && unlink $tf
@@ -72,11 +91,14 @@ trap rm_tf EXIT
 cp $ALACRITTY_BINARY $tf
 chmod +x $tf
 
+set -x
 if [[ -f "$APP_ICON_ICNS" ]]; then
   seticon $APP_ICON_ICNS $tf
 fi
 
+geticon $tf .i && icns2image .i .i.png && cat .i.png | imgcat && unlink .i && unlink .i.png
 
-cmd="$tf --config-file $ALACRITTY_CONFIG -owindow.decorations=transparent -owindow.startup_mode=$ALACRITTY_WINDOW_STARTUP_MODE -ofont.normal.family=\"$ALACRITTY_FONT\" -owindow.position.x=$X -owindow.position.y=$Y -owindow.dimensions.columns=$COLS -owindow.dimensions.lines=$LINES"
+cmd="$tf --config-file $ALACRITTY_CONFIG"
+#-owindow.decorations=transparent -owindow.startup_mode=$ALACRITTY_WINDOW_STARTUP_MODE -ofont.normal.family=\"$ALACRITTY_FONT\" -owindow.position.x=$X -owindow.position.y=$Y -owindow.dimensions.columns=$COLS -owindow.dimensions.lines=$LINES -ofont.normal.style=$FONT_STYLE"
 echo -e "$cmd"
 eval $cmd
---
+++ list_monospaced_fonts.sh
@@ -0,0 +1,2 @@
+fc-list : family spacing outline scalable | grep -e spacing=100 -e spacing=90 | grep -e outline=True | grep -e scalable=True
+
